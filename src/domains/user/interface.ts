1fd3fbbcaf1cffa789ccd98893fd3101a9e24ec3
Apisit Bucha <apisit_buc@true-e-logistics.com>
2022-04-02 00:43:28 +0700

    interface file

---
+++ src/domains/user/interface.ts
@@ -0,0 +1,11 @@
+export interface IUserSignUp {
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+}
+
+export interface ILogin {
+  username: string;
+  password: string;
+}
